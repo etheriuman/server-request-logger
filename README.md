04066fd0b08d6c53a2ab380686739ba9fbfbf7cd
etheriuman <timmy26291125@gmail.com>
2020-11-27 01:14:36 +0800

    upload everything at once

---
+++ README.md
@@ -0,0 +1,41 @@
+# 小實驗： server request logger
+  
+---
+- ## 運行環境需求:
+  Node.js
+
+- ## 插件需求 :
+1. npm
+2. Express
+3. Nodemon
+
+- ## 功能 :
+  在每次伺服器收到請求時都會列出日期、時間、請求方法與路由。
+
+- ## 應用程式安裝前導 :
+1. ### 請用 Mac users: 終端機 / Windows users: Git Bash 輸入以下內容：
+
+2. 將專案從GitHub複製到你的本機中：
+```
+git clone https://github.com/etheriuman/middleware-time-spent.git
+```
+3. 進入到該資料夾中：
+```
+cd middleware-time-spent
+```
+4. 安裝npm：
+```
+npm init -y
+```
+5. 用npm指令安裝所有所需的插件：
+```
+npm i
+```
+6. 開始運行伺服器：
+```
+npm run dev
+```
+7. 將回傳的網址貼到瀏覽器中：
+```
+App running on http://localhost:3000
+```
---
+++ app.js
@@ -0,0 +1,70 @@
+// app.js
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+// request function middleware (first middleware)
+app.use( function (req, res, next) {
+
+  // 請求的url
+  const currentRouter = req.originalUrl
+
+  // 請求的方法
+  const method = req.method
+
+  // 請求的時間
+  const requestTimestamp =  new Date(Date.now())
+
+  // 把 timestamp 傳給全域變數 startTimestamp
+  startTomestamp = requestTimestamp
+
+  const YYYY = requestTimestamp.getFullYear()
+  const MM = requestTimestamp.getMonth()
+  const DD = requestTimestamp.getDate()
+  const hr = requestTimestamp.getHours()
+  const min = requestTimestamp.getMinutes()
+  const sec = requestTimestamp.getSeconds()
+
+  // 輸出字串
+  const startTimeOutPrint = `${YYYY}-${MM}-${DD} ${hr}:${min}:${sec}`
+
+  // log start time
+  console.log(`${startTimeOutPrint} | ${method} from '${currentRouter}'`)
+
+  // 結束 middleware 
+  next()
+
+})
+
+
+
+// 路由設定 (second and the last middleware)
+
+// 這邊因為沒response一個完整的html結構，瀏覽器會預設自己去找 /favicon.ico檔案，以至於 double request
+// --> 所以給 browser 一個 site icon data 防止瀏覽器重複送出請求
+const iconData = '<link rel="icon" href="data:,">'
+
+app.get('/', (req, res) => {
+    res.send(iconData + '列出全部 Todo')
+})
+
+app.get('/new', (req, res) => {
+  res.send(iconData + '新增 Todo 頁面')
+})
+ 
+app.get('/:id', (req, res) => {
+  res.send(iconData + '顯示一筆 Todo')
+})
+
+app.post('/', (req, res) => {
+  res.send(iconData + '新增一筆  Todo')
+})
+
+
+
+// 啟動伺服器，伺服器等待中...
+app.listen(port, () => {
+  console.log(`App running on http://localhost${port}`)
+})
